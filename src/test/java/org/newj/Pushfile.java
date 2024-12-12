b974e1a973d03bea2223646595716840068241ea
1992Arun <arunkumar.endiyur@gmail.com>
2024-12-12 21:50:26 +0530

    new code

---
+++ src/test/java/org/newj/Pushfile.java
@@ -0,0 +1,19 @@
+package org.newj;
+
+public class Pushfile {
+	
+	public static void main(String[] args) {
+		
+		print();
+		
+	}
+	
+	public static void print() {
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+		
+	}
+
+}
